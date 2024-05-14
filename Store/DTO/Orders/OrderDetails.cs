6d150a443e8263436562541300e3101030660e2e
Aydar U. <aidohha@gmail.com>
2024-05-15 00:31:53 +0500

    add

---
+++ Store/DTO/Orders/OrderDetails.cs
@@ -0,0 +1,15 @@
+﻿using Store.DTO.Accounts;
+using Store.DTO.Products;
+using Store.Models;
+
+namespace Store.DTO.Orders;
+
+public record OrderDetails(
+    int Id,
+    int OrderId,
+    int ProductId,
+    double Price,
+    int Quantity,
+    DateTime CreateDate,
+    LoginResponse User,
+    string Address);
---
+++ Store/Models/OrderDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Models;
+
+public class OrderDetails
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+    
+    public Order Order { get; set; }
+    public Product Product { get; set; }
+}
